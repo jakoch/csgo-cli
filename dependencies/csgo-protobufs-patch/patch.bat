ddc2220ee5841ca08422bdb146b552c56e0a4969
jakoch <jakoch@web.de>
2018-03-19 22:08:37 +0100

    force file xcopy

--- dependencies/csgo-protobufs-patch/patch.bat
+++ dependencies/csgo-protobufs-patch/patch.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM 1. copy the Protobuf Descriptor file
-xcopy "C:\Program Files (x86)\protobuf\include\google\protobuf\descriptor.proto" ..\csgo-protobufs\csgo\google\protobuf\descriptor.proto
+ECHO F|xcopy "C:\Program Files (x86)\protobuf\include\google\protobuf\descriptor.proto" ..\csgo-protobufs\csgo\google\protobuf\descriptor.proto
 
 REM 2. proto-syntax-patch.txt contains 1 header line
 copy proto-syntax-patch.txt ..\csgo-protobufs\csgo
