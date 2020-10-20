07e94723ef4aa7a525472e7cd461b66c3e3daaf1
Jens A. Koch <jakoch@web.de>
2020-10-21 01:03:03 +0200

    readd csgo-protobufs-patch (to get rid of the messages in appveyor)

---
+++ dependencies/csgo-protobufs-patch/patch.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM 1. copy the Protobuf Descriptor file
+mkdir "%~dp0..\csgo-protobufs\csgo\google\protobuf"
+copy "%~dp0..\..\vcpkg_installed\x64-windows-static\include\google\protobuf\descriptor.proto" "%~dp0..\csgo-protobufs\csgo\google\protobuf"
+
+REM 2. proto-syntax-patch.txt contains 1 header line
+copy "%~dp0proto-syntax-patch.txt" "%~dp0..\csgo-protobufs\csgo"
+
+REM add an extension to every file
+cd "%~dp0..\csgo-protobufs\csgo"
+dir
+
+for %%f in (*.proto) do (
+    echo "Processing %%f"
+    copy proto-syntax-patch.txt %%f.new
+    REM echo "Appending original file content to file with first Line %%f.new"
+    type %%f >>%%f.new
+    REM echo "Rename/overwriting old file"
+    move /y %%f.new %%f
+)
