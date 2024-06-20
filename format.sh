f6c812b47befd1c2db90949db9460da185e9139d
Jens A. Koch <jakoch@web.de>
2024-06-20 22:13:27 +0200

    require clang-18

--- format.sh
+++ format.sh
@@ -3,10 +3,10 @@
 # allow ENV.CLANG_FORMAT to define the path to the binary or default to clang-format
 CLANG_FORMAT=${CLANG_FORMAT:-clang-format}
 
-# require clang-format 17 to have consistent formatting features
+# require clang-format 18 to have consistent formatting features
 $CLANG_FORMAT --version
-if [[ ! $($CLANG_FORMAT --version) =~ "version 17" ]]; then
-    echo "Error: clang-format version must be 17"
+if [[ ! $($CLANG_FORMAT --version) =~ "version 18" ]]; then
+    echo "Error: clang-format version must be 18"
     exit 1
 fi
 
