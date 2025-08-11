aeb9002f84d8619af69f4203d4437d288957583f
Jens A. Koch <jakoch@web.de>
2025-08-11 16:50:12 +0200

    rename SetupVcpkg.cmake

--- cmake/SetupVcpkg.cmake
+++ cmake/SetupVcpkg.cmake
@@ -72,36 +72,14 @@ if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
     set(CMAKE_TOOLCHAIN_FILE ${vcpkg_toolchain_file})
 endif()
 
-#
-# -- VCPKG_DEFAULT_TRIPLET
-#
-if(DEFINED ENV{VCPKG_DEFAULT_TRIPLET} AND NOT DEFINED VCPKG_TARGET_TRIPLET)
-  set(VCPKG_TARGET_TRIPLET "$ENV{VCPKG_DEFAULT_TRIPLET}" CACHE STRING "")
-endif()
-
-#
-# -- VCPKG_DIR
-#
-# VCPKG_DIR is the root folder for all compiled packages, e.g.
-# the local /project/vcpkg_packages/x64-windows
-# or the global /opt/vcpkg/packages/x64-windows.
-#
-# Because finding dependencies automatically is still on the todo list of vcpkg, we need to guide it.
-#
-# Please use this variable to point to the locations of your packages share folder like below:
-# set(spdlog_DIR "${VCPKG_DIR}/share/spdlog")
-# find_package(spdlog CONFIG REQUIRED)
-#
-if(NOT DEFINED VCPKG_DIR)
-    if(WIN32)
-        set(VCPKG_DIR "${CMAKE_SOURCE_DIR}/vcpkg_packages/${VCPKG_TARGET_TRIPLET}")
-    else()
-        set(VCPKG_DIR "${CMAKE_SOURCE_DIR}/build/${CMAKE_BUILD_TYPE}/vcpkg_packages/${VCPKG_TARGET_TRIPLET}")
-    endif()
-endif()
-
 iF(NOT DEFINED VCPKG_MANIFEST_FILE)
     set(VCPKG_MANIFEST_FILE "${CMAKE_SOURCE_DIR}/vcpkg.json")
+endif()
+
+# Must be before any find_package() calls
+if(NOT DEFINED VCPKG_TARGET_TRIPLET)
+    # Choose triplet here:
+    set(VCPKG_TARGET_TRIPLET "x64-windows-static" CACHE STRING "Vcpkg target triplet")
 endif()
 
 # Add this file and the VCPKG_MANIFEST_FILE as a "vcpkg" source_group to the IDE.
@@ -134,8 +112,6 @@ message(STATUS "[VCPKG]  - VCPKG_VERBOSE             -> '${VCPKG_VERBOSE}'")
 message(STATUS "[VCPKG]  - VCPKG_APPLOCAL_DEPS       -> '${VCPKG_APPLOCAL_DEPS}'")
 message(STATUS "[VCPKG]  - E:VCPKG_FEATURE_FLAGS     -> '$ENV{VCPKG_FEATURE_FLAGS}'")
 message(STATUS "[VCPKG]  - E:VCPKG_ROOT              -> '$ENV{VCPKG_ROOT}'")
-message(STATUS "[VCPKG]  - E:VCPKG_INSTALLATION_ROOT -> '$ENV{VCPKG_INSTALLATION_ROOT}'")
 message(STATUS "[VCPKG]  - CMAKE_TOOLCHAIN_FILE      -> '${CMAKE_TOOLCHAIN_FILE}'")
 message(STATUS "[VCPKG]  - VCPKG_MANIFEST_FILE       -> '${VCPKG_MANIFEST_FILE}'")
 message(STATUS "[VCPKG]  - VCPKG_TARGET_TRIPLET      -> '${VCPKG_TARGET_TRIPLET}'")
-message(STATUS "[VCPKG]  - VCPKG_DIR                 -> '${VCPKG_DIR}'")
